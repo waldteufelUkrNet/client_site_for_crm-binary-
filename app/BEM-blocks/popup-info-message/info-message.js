4c21591565dda37d86fc976682b5a9ea06f2d8b7
waldteufelUkrNet <waldteufel@ukr.net>
2018-10-07 23:16:45 +0300

    homework 2018.10.07

--- app/BEM-blocks/popup-info-message/info-message.js
+++ app/BEM-blocks/popup-info-message/info-message.js
@@ -1,24 +1,20 @@
-$(document).ready(function(){
+function showInfoMessage(){
+	$('.info-message').css({'right':'0px'});
 
-  $('.tempBTN2').click(function(){
-  	$('.info-message').css({'right':'0px'});
+	var closeInfoPopup = setTimeout(function(){
+		$('.info-message').css({'right':'-290px'});
+	},4000);
 
-  	var closeInfoPopup = setTimeout(function(){
-  		$('.info-message').css({'right':'-290px'});
-  	},4000);
-
-	  $('.info-message__close-btn').click(function(){
-	  	$('.info-message').css({'right':'-290px'});
-	  	clearTimeout(closeInfoPopup);
-	  });
-
-	  $('.info-message').mouseenter(function(){
-	  	clearTimeout(closeInfoPopup);
-	  });
-
-	  $('.info-message').mouseleave(function(){
-	  	$('.info-message').css({'right':'-290px'});
-	  });
+  $('.info-message__close-btn').click(function(){
+  	$('.info-message').css({'right':'-290px'});
+  	clearTimeout(closeInfoPopup);
   });
 
-});
+  $('.info-message').mouseenter(function(){
+  	clearTimeout(closeInfoPopup);
+  });
+
+  $('.info-message').mouseleave(function(){
+  	$('.info-message').css({'right':'-290px'});
+  });
+}
