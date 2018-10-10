088bbe2fa8250dc8f55ce8dcc7d953fd39e5da1f
waldteufel <waldteufel@ukr.net>
2018-10-10 17:53:11 +0300

    officework 2018.10.10

--- app/js-expanded/js.js
+++ app/js-expanded/js.js
@@ -38,8 +38,14 @@ $('#investment-input').bind('blur keyup', function(event) {
   var inputValue   = +$('#investment-input').val(),
       percentValue = +$('#investment-percent').text();
 
-  var resultValue  = (inputValue * percentValue)/100 + inputValue;
+  if (inputValue == 0) {
+    // прибирання підсвіток
+    $('.parlay-slider__parlay-choise-btn').css('background-color','transparent');
+    // блокування кнопок для активації ставки, якщо ставка == 0
+    $('.parlay-btns__cover').css('display','flex');
+  }
 
+  var resultValue  = (inputValue * percentValue)/100 + inputValue;
   if ( !isNumeric(resultValue) ) {
     resultValue = 'ошибка ввода!';
     $( $('#investment-input') ).val('').css({'border':'1px solid red'});
@@ -77,8 +83,134 @@ setInterval(function() {
 }, 1000);
 /* ↑↑↑ /datetimer ↑↑↑ */
 
+/* ↓↓↓ parlay-slider ↓↓↓ */
+// $('.parlay-accordion__btn').click(function(){
+
+//   // контроль можливості торгівлі акціями (торги на них не цілодобові)
+//   var date = new Date;
+//   var currentTimeInMinutes = date.getUTCHours() * 60 + date.getUTCMinutes(),
+//       minTimeInMinutes = 10 * 60 + 00,
+//       maxTimeInMinutes = 14 * 60 + 00;
+//   if( $( $('.slick-current').children('.wares-slider__item-header')[0] ).text() == 'Акции'
+//       && (currentTimeInMinutes < minTimeInMinutes || currentTimeInMinutes > maxTimeInMinutes) ) {
+//     showInfoMessage('Акционная биржа на данный момент закрыта. Торговать акциями возможно только с 10:00 до 17:00\
+//                      по Гринвичу (часовой пояс UTC)');
+//     return
+//   }
+//   console.log('тут код для акордеона');
+
+// });
+/* ↑↑↑ /parlay-slider ↑↑↑ */
+
+var parlayInvestment,     // розмір ставки
+    parlayType,           // short/normal/long
+    parlayTime,           // час ДО закриття ставки - в секундах
+    parlayAnticipation,   // up/down
+    parlayPairName,       // назва пари
+    parlayCurrentPrice;   // поточна котировка
+
+var TradingHolidays  = [ '2018-11-22', '2018-12-25', '2019-01-01', '2019-01-21', '2019-02-18', '2019-04-19',
+                         '2019-05-27', '2019-07-04', '2019-09-02', '2019-11-28', '2019-12-25', '2020-01-01',
+                         '2020-01-20', '2020-02-17', '2020-04-10', '2020-05-25', '2020-07-03', '2020-09-07',
+                         '2020-11-26', '2020-12-25'
+                       ];
+// Market closes at 1:00 p.m. ET.
+var TradingShortDays = [ '2018-11-23', '2018-12-24', '2019-07-03', '2019-11-29', '2019-12-24', '2020-11-27',
+                         '2020-12-24'
+                       ];
+
+/* ↓↓↓ динамічне формування списків можливих ставок ↓↓↓ */
+$( $('.parlay-slider').children('.slick-arrow') ).click(function(){
+
+  parlayType = $( $('.parlay-slider').find('.slick-current')[0] ).attr('data-parlayType');
+  if (parlayType == 'normal') {
+    var tempDateTime   = new Date,
+        tempUTCMinutes = tempDateTime.getUTCMinutes(),
+        tempUTCFullYear,
+        tempUTCMonth,
+        tempUTCDate,
+        tempUTCHours;
+
+
+
+    // округлення часу ставок до 00хв або 30хв
+    if ( 25 <= tempUTCMinutes && tempUTCMinutes < 55 ) {
+      // оркуглити до 00, додати 1 годину
+      tempDateTime.setUTCMinutes(60);
+    } else if ( 0 <= tempUTCMinutes && tempUTCMinutes < 25 ) {
+      // оркуглити до 30
+      tempDateTime.setUTCMinutes(30);
+    } else if ( 55 <= tempUTCMinutes && tempUTCMinutes <= 59 ) {
+      // округлити до 30, додати годину
+      tempDateTime.setUTCMinutes(90);
+    }
+
+    console.log("tempDateTime", tempDateTime);
+    var tempUTCDate = date.getUTCDate();
+    if (tempUTCDate < 10) tempUTCDate = '0' + tempUTCDate;
+
+    var tempUTCMonth = date.getUTCMonth() + 1;
+    if (tempUTCMonth < 10) tempUTCMonth = '0' + tempUTCMonth;
+
+    var yy = date.getUTCFullYear();
+    if (tempUTCFullYear < 10) tempUTCFullYear = '0' + tempUTCFullYear;
+
+    var tempUTCHours = date.getUTCHours();
+    if (tempUTCHours < 10) tempUTCHours = '0' + tempUTCHours;
+
+    if (tempUTCMinutes < 10) tempUTCMinutes = '0' + tempUTCMinutes;
+
+    //datetimer.innerHTML = dd + "." + mm + "." + yy + "   " + hh + ":" + mn + ":" + ss;
+
+    tempDateTimeString = tempDateTime + '';
+    console.log("tempDateTimeString", tempDateTimeString);
+
+    $('.parlay-slider__item[data-parlayType="normal"]').children('.parlay-slider__item-choice-field')
+                                                       .children('.parlay-slider__parlay-choise-btn-holder')
+                                                       .append('<div class="parlay-slider__parlay-choise-btn">'
+                                                                    + tempDateTime +
+                                                               '</div>');
+
+  }
+  // прибирання підсвіток
+  $('.parlay-slider__parlay-choise-btn').css('background-color','transparent');
+  // деактивувати кнопки ставок
+  $('.parlay-btns__cover').css('display','flex');
+});
+/* ↑↑↑ /динамічне формування списків можливих ставок ↑↑↑ */
+
+/* ↓↓↓ попередній вибір ставки ↓↓↓ */
+$('.parlay-slider__parlay-choise-btn').click(function(){
+  // якщо є якесь значення ставки
+  if ( +$('#investment-input').val() > 0 ) {
+    // підсвітка по кліку
+    $('.parlay-slider__parlay-choise-btn').css('background-color','transparent');
+    $(this).css('background-color','rgba(0,0,0,.3)');
+    // активувати кнопки ставок
+    $('.parlay-btns__cover').css('display','none');
+  }
+});
+/* ↑↑↑ /попередній вибір ставки ↑↑↑ */
+
 /* ↓↓↓ create active-slider-item ↓↓↓ */
-$('.tempBTN').click(function(){
+$('.parlay-btns__btn').click(function(){
+  // parlayTime,           // час ДО закриття ставки - в секундах
+  parlayInvestment = +$('#investment-input').val();
+  //console.log("parlayInvestment", parlayInvestment);
+  parlayType = $( $('.parlay-slider').find('.slick-current')[0] ).attr('data-parlayType');
+  //console.log("parlayType", parlayType);
+  parlayAnticipation = $(this).attr('data-parlayAnticipation');
+  //console.log("parlayAnticipation", parlayAnticipation);
+  parlayPairName = $('.trade-pair__name').text();
+  //console.log("parlayPairName", parlayPairName);
+  parlayCurrentPrice = $('.trade-pair__price').text();
+  //console.log("parlayCurrentPrice", parlayCurrentPrice);
+});
+//createParlay(args);
+/* ↑↑↑ /create active-slider-item ↑↑↑ */
+
+/* ↓↓↓ FUNCTIONS DECLARATIONS ↓↓↓ */
+function createParlay(){
   // активних ставок - не більше 10
   if( $('#active-slider .active-slider__item').not('.slick-cloned').length >= 10 ) {
     showInfoMessage("Вы достигли максимально возможного количества одновременно открытых ставок.\
@@ -107,6 +239,12 @@ $('.tempBTN').click(function(){
                                       <div class="active-slider__item-graphic">'
                                         + tempNumber +
                                       '</div>\
+                                      <div class="active-slider__info">\
+                                          <span class="active-slider__pair-name">BTC/ETH</span>\
+                                          <span class="active-slider__parlay"> 30 </span>\
+                                              <i class="fas fa-angle-double-up" style="color:dodgerblue"></i>\
+                                          <div class="active-slider__current-price">12345.67890</div>\
+                                      </div>\
                                   </div>');
     // перезапустити slick
     $( '#active-slider' ).slick({
@@ -146,53 +284,8 @@ $('.tempBTN').click(function(){
     });
 
   }
-});
-/* ↑↑↑ /create active-slider-item ↑↑↑ */
+}
 
-/* ↓↓↓ parlay-slider ↓↓↓ */
-// $('.parlay-accordion__btn').click(function(){
-
-//   // контроль можливості торгівлі акціями (торги на них не цілодобові)
-//   var date = new Date;
-//   var currentTimeInMinutes = date.getUTCHours() * 60 + date.getUTCMinutes(),
-//       minTimeInMinutes = 10 * 60 + 00,
-//       maxTimeInMinutes = 14 * 60 + 00;
-//   if( $( $('.slick-current').children('.wares-slider__item-header')[0] ).text() == 'Акции'
-//       && (currentTimeInMinutes < minTimeInMinutes || currentTimeInMinutes > maxTimeInMinutes) ) {
-//     showInfoMessage('Акционная биржа на данный момент закрыта. Торговать акциями возможно только с 10:00 до 17:00\
-//                      по Гринвичу (часовой пояс UTC)');
-//     return
-//   }
-//   console.log('тут код для акордеона');
-
-// });
-var parlayType,
-    parlayTime,
-    parlayPeriod;
-$( $('.parlay-slider').children('.slick-arrow') ).click(function(){
-  // прибирання підсвіток
-  var tempArr = $('.parlay-slider__parlay-choise-btn');
-  for (var i = 0; i < tempArr.length; i++) {
-    $(tempArr[i]).css('background-color','transparent');
-  }
-  // деактивувати кнопки ставок
-  $('.parlay-btns__cover').css('display','flex');
-});
-
-$('.parlay-slider__parlay-choise-btn').click(function(){
-  // підсвітка по кліку
-  var tempArr = $('.parlay-slider__parlay-choise-btn');
-  for (var i = 0; i < tempArr.length; i++) {
-    $(tempArr[i]).css('background-color','transparent');
-  }
-  $(this).css('background-color','rgba(0,0,0,.3)');
-  // активувати кнопки ставок
-  $('.parlay-btns__cover').css('display','none');
-
-});
-/* ↑↑↑ /parlay-slider ↑↑↑ */
-
-/* ↓↓↓ FUNCTIONS DECLARATIONS ↓↓↓ */
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n)
 }
